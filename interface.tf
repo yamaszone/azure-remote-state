b980ae58d77eba58cd01858ac1aad711c2f7eadc
Steven Zinck <szinck@gmail.com>
2018-05-03 12:07:39 -0300

    initial commit

---
+++ interface.tf
@@ -0,0 +1,33 @@
+variable "name" { 
+  description = "Name of resource"
+}
+
+variable "location" {
+  default = "canadaeast"
+}
+
+variable "resource_group_name" {
+  description = "Name of resource group"
+}
+
+variable "account_tier" {
+  default = "standard"
+}
+
+
+variable "storage_account_replication_type" {
+  default     = "LRS"
+}
+
+variable "prefix" {
+  default = "cds"
+}
+
+variable "environment" {
+  default = "nonprod"
+}
+
+variable "storage_account_name" {
+  default = "cdsremotestate"
+}
+
